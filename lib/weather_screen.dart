a9079377ea2f9f2fa9142adb4de30fbfe43ff82f
gopalkrushnas063 <gopalkrushnas063@gmail.com>
2023-09-08 10:38:45 +0530

    created separate stateless widget for separate item view also design for additional info section

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:wheather_app_flutter/addition_info_item.dart';
+import 'package:wheather_app_flutter/hourly_forecast_item.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -81,7 +83,7 @@ class WeatherScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 8),
             //weather forecast card
             const SingleChildScrollView(
               scrollDirection: Axis.horizontal,
@@ -111,25 +113,11 @@ class WeatherScreen extends StatelessWidget {
 
             const SizedBox(height: 16),
             const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Column(
-                  children: [
-                    Icon(
-                      Icons.water_drop,
-                      size: 32,
-                    ),
-                    SizedBox(height: 8),
-                    Text('Humidity'),
-                    SizedBox(height: 8),
-                    Text(
-                      '94',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ],
-                ),
+                AdditionalInfoItem(),
+                AdditionalInfoItem(),
+                AdditionalInfoItem(),
               ],
             ),
           ],
@@ -139,40 +127,3 @@ class WeatherScreen extends StatelessWidget {
   }
 }
 
-class HourlyForecastItem extends StatelessWidget {
-  const HourlyForecastItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 6,
-      child: Container(
-        width: 100,
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: const Column(
-          children: [
-            Text(
-              '03:00',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
-            Icon(
-              Icons.cloud,
-              size: 32,
-            ),
-            SizedBox(height: 8),
-            Text(
-              '320.12',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
